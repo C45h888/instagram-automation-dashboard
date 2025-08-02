2b3c2e993112b8c169297178986ded35df938d74
C45h888 <kamransait41@gmail.com>
2025-08-02 09:32:37 +0530

    Updated main.tsx

--- instagram-automation-dashboard/src/main.tsx
+++ instagram-automation-dashboard/src/main.tsx
@@ -1,15 +1,11 @@
+// src/main.tsx - FIXED (remove QueryClientProvider since App.tsx now has it)
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <App />
   </React.StrictMode>
 );
